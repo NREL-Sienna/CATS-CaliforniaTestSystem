00a119b5098c1c4db57b05279674717ca85287fb
Jose Daniel Lara <jdlara@berkeley.edu>
2025-01-02 17:16:06 -0700

    add parallel run files

--- Script/test_eval_parallel_simplified.jl
+++ Script/test_eval_parallel_simplified.jl
@@ -6,7 +6,7 @@ addprocs(30)
     using Pkg
     Pkg.activate(CATS_DIR)
     #Pkg.develop(path = "/home/jlara/HSL_jll.jl-2023.11.7")
-    #using HSL_jll
+    # using HSL_jll
     using PowerModels
     using JuMP
     using CSV, JSON
@@ -19,8 +19,8 @@ addprocs(30)
     function eval(range, NetworkData_input, load_scenarios, load_mapping, HourlyData2019, gen_data)
         solver = JuMP.optimizer_with_attributes(() -> Ipopt.Optimizer(),
             "print_level" => 3,
-            #"hsllib" => HSL_jll.libhsl_path,
-            #"linear_solver" => "ma27"
+            # "hsllib" => HSL_jll.libhsl_path,
+            # "linear_solver" => "ma27"
         )
 
         NetworkData = deepcopy(NetworkData_input)
@@ -59,7 +59,7 @@ addprocs(30)
                 @error "$k in range $range infeasible skipping write"
             else
                 tmp_condenserReactiveFlows = Tables.table([k, [round(solution["solution"]["gen"]["$x"]["qg"], digits = 4) for x in condenserIndices]...]')
-                CSV.write("condenserReactiveFlows.csv", tmp_condenserReactiveFlows; append=true)
+                CSV.write("SimplifiedcondenserReactiveFlows.csv", tmp_condenserReactiveFlows; append=true)
                 @info "Wrote case $k in range $range"
             end
         end
@@ -71,14 +71,14 @@ splits = [range(N, N + split_N - 1) for N in 1:split_N:8760]
 
 load_scenarios = CSV.read("$DATA_DIR/Load_Agg_Post_Assignment_v3_latest.csv",header = false, DataFrame)
 HourlyData2019 = CSV.read("$DATA_DIR/HourlyProduction2019.csv",DataFrame)
-gen_data = CSV.read("$CATS_DIR/GIS/CATS_gens.csv",DataFrame)
+gen_data = CSV.read("$CATS_DIR/GIS/SimplifiedCATS_gens.csv",DataFrame)
 condenserIndices = [g for g in 1:size(gen_data)[1] if occursin("condenser", lowercase(gen_data.FuelType[g]))]
 
 condenserReactiveFlows = DataFrame("timestep" => Int[],
 ("gen $x" => Float64[] for x in condenserIndices)...)
 CSV.write("condenserReactiveFlows.csv", condenserReactiveFlows)
 
-NetworkData = PowerModels.parse_file("$CATS_DIR/MATPOWER/CaliforniaTestSystem.m")
+NetworkData = PowerModels.parse_file("$CATS_DIR/MATPOWER/SimplifiedCaliforniaTestSystem.m")
 load_mapping = map_buses_to_loads(NetworkData)
 
 @info "finished reading the data"
